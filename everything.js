3644a0a6cd23773a5d7083deece10b18d7bfcdb7
Shakeel Mohamed <shakeel@shakeelmohamed.com>
2016-04-16 16:43:31 -0700

    Initial commit

---
+++ everything.js
@@ -0,0 +1,58 @@
+var yt_key = "AIzaSyDf0NS5Pzvra51I3nzDsjTzLUVzqIrcbJU";
+var oauth2_client = "1086911666386-a83cmtgp50komi85mtdc2agued35e7bn.apps.googleusercontent.com";
+
+function euc(s) {
+    return encodeURIComponent(s);
+}
+
+function auth() {
+    window.location.href = "https://accounts.google.com/o/oauth2/auth" + "?client_id=" + euc(oauth2_client) + "&response_type=token" + "&scope=" + euc("https://www.googleapis.com/auth/youtube.readonly") + "&mine=true" + "&redirect_uri=" + euc(window.location.href + "welcome.html");
+}
+
+function getChannelInfo(token, onData, onFail) {
+    $.getJSON("https://www.googleapis.com/youtube/v3/channels", {
+        key: yt_key,
+        part: "contentDetails",
+        mine: true,
+        access_token: token
+    }, onData).fail(onFail);
+}
+
+// TODO: pagination, get data for all videos
+function watchLater(token, watchLaterID, page, onData, onFail) {
+    // return "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=" + watchLaterID + "&key=" + yt_key;
+    var args = {
+        key: yt_key,
+        part: "snippet",
+        maxResults: 50,
+        playlistId: watchLaterID,
+        access_token: token
+    };
+
+    if (page) {
+        args.pageToken = page;
+    }
+
+    $.getJSON("https://www.googleapis.com/youtube/v3/playlistItems", args, onData).fail(onFail);
+}
+
+// $(function() {
+//     auth(
+//         function(data) {
+//             console.log(data);
+//         },
+//         function(err) {
+//             console.log("err");
+//             console.log(err);
+//         }
+//     );
+// });
+    // getChannelInfo(
+    //     function(data) {
+    //         console.log(data);
+    //     },
+    //     function(err) {
+    //         console.log("err");
+    //         console.log(err);
+    //     }
+    // );
